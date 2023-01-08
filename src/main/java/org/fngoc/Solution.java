b1b87a47ff2da3941c620e0cf365ee8a18d90296
VitMarKha <vitmarkha@icloud.com>
2023-01-08 15:24:45 +0300

    add tasks in five sprint

--- src/main/java/org/fngoc/Solution.java
+++ src/main/java/org/fngoc/Solution.java
@@ -7,6 +7,42 @@ import java.util.stream.Collectors;
 
 public class Solution {
 
+    ///////////////////Просеивание вниз///////////////////
+    public static int siftDown(int[] heap, int idx) {
+        int left = idx * 2;
+        int right = idx * 2 + 1;
+        int large = left;
+
+        if (left >= heap.length)
+            return idx;
+
+        if (right < heap.length && heap[left] < heap[right])
+            large = right;
+
+        if (heap[idx] < heap[large]) {
+            int tmp = heap[idx];
+            heap[idx] = heap[large];
+            heap[large] = tmp;
+            return siftDown(heap, large);
+        }
+        return idx;
+    }
+
+    ///////////////////Просеивание вверх///////////////////
+    public static int siftUp(int[] heap, int idx) {
+        int root = idx / 2;
+
+        if (idx == 1 || heap[root] > heap[idx])
+            return idx;
+
+        if (heap[root] < heap[idx]) {
+            int tmp = heap[idx];
+            heap[idx] = heap[root];
+            heap[idx / 2] = tmp;
+        }
+        return siftUp(heap, idx / 2);
+    }
+
     ///////////////////Сбалансированное дерево///////////////////
     private static boolean flag = true;
 
