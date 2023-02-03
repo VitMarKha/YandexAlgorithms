9751aa1e25c6c48b7f776462f27f5c736f761140
VitMarKha <vitmarkha@icloud.com>
2023-02-03 18:05:01 +0700

    add final tasks, in six sprint

--- src/main/java/org/fngoc/finaltasks/sprint_6/Railways.java
+++ src/main/java/org/fngoc/finaltasks/sprint_6/Railways.java
@@ -5,103 +5,56 @@ import java.util.*;
 
 public class Railways {
 
-    private static class Road implements Comparable<Road> {
-
-        private final int city;
-        private final char railway;
-
-        public Road(int city, char railway) {
-            this.city = city;
-            this.railway = railway;
-        }
-
-        public int getCity() {
-            return city;
-        }
-
-        public int getRailway() {
-            return railway;
-        }
-
-        @Override
-        public String toString() {
-            return "Road{" +
-                    "city=" + city +
-                    ", railway=" + railway +
-                    '}';
-        }
-
-        @Override
-        public int compareTo(Road o) {
-            return this.getCity() - o.getCity();
-        }
-    }
-
     private static int capital;
-//    private static int[] colors;
-    private static boolean[] visited;
-    private static Map<Integer, Set<Road>> vertexes;
+    private static int[] colors;
+    private static Map<Integer, List<Integer>> vertexes;
 
     public static void main(String[] args) throws IOException {
         input();
         if (capital == 1)
             System.out.println("YES");
-        else
-            output(findWayBFS('B'), findWayBFS('R'));
-//            output(findWayDFS('B'), findWayDFS('R'));
+        else {
+            if (!findCycleDFS())
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
     }
 
-//    private static boolean findWayDFS(char railway) {
-//        for (int i = 1; i < visited.length; i++) {
-//            if (!visited[i]) {
-//                if (isCycleDFS(i, railway))
-//                    return true;
-//                else
-//                    isCycleDFS(i, railway);
-//            }
-//        }
-//        return true;
-//    }
-//
-//    private static boolean isCycleDFS(int city, char railway) {
-//        if (city == capital) {
-//            System.out.println("IS DONE CAPITAL");
-//            return false;
-//        }
-//        visited[city] = true;
-//
-//        Set<Road> roads = vertexes.get(city);
-//
-//        for (Road road : roads) {
-//            if (!visited[road.getCity()] && road.getRailway() == railway)
-//                isCycleDFS(road.getCity(), railway);
-//            else {
-//                System.out.println("cycle");
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-
-    //colors[road.getCity()] == 0 &&
-
-    private static boolean findWayBFS(char railway) {
-        Queue<Integer> planned = new ArrayDeque<>();
-        planned.add(1);
-
-        while (!planned.isEmpty()) {
-            int currentCity = planned.remove();
-            if (currentCity == capital)
-                return true;
-
-            Set<Road> roads = vertexes.get(currentCity);
-            if (roads == null)
-                continue;
-
-            for (Road road : roads) {
-                if (road.getRailway() == railway)
-                    planned.add(road.getCity());
+    private static boolean findCycleDFS() {
+        for (int i = 1; i < capital; i++) {
+            if (colors[i] == 0) {
+                if (isCycle(i))
+                    return true;
             }
+        }
+        return false;
+    }
+
+    private static boolean isCycle(int city) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(city);
+
+        while (!stack.isEmpty()) {
+            int currentCity = stack.pop();
+
+            if (colors[currentCity] == 0) {
+                colors[currentCity] = 1;
+                stack.push(currentCity);
+
+                List<Integer> roads = vertexes.get(currentCity);
+
+                if (roads == null)
+                    continue;
+
+                for (Integer road : roads) {
+                    if (colors[road] == 0)
+                        stack.push(road);
+                    else if (colors[road] == 1)
+                        return true;
+                }
+            } else if (colors[currentCity] == 1)
+                colors[currentCity] = 2;
         }
         return false;
     }
@@ -112,31 +65,32 @@ public class Railways {
             capital = Integer.parseInt(tokenizer.nextToken());
 
             vertexes = new HashMap<>();
-            visited = new boolean[capital + 1];
+            colors = new int[capital + 1];
 
-            for (int i = 1; i < capital; i++) {
+            for (int i = 0; i < capital - 1; i++) {
                 tokenizer = new StringTokenizer(reader.readLine());
                 String line = tokenizer.nextToken();
 
-                Set<Road> roads = new TreeSet<>();
-                int nextCityIterator = 1;
                 for (int j = 0; j < line.length(); j++) {
-                    roads.add(new Road(i + nextCityIterator, line.charAt(j)));
-                    nextCityIterator += 1;
+                    if (line.charAt(j) == 'B') {
+                        if (vertexes.containsKey(i))
+                            vertexes.get(i).add(i + j + 1);
+                        else {
+                            List<Integer> list = new ArrayList<>();
+                            list.add(i + j + 1);
+                            vertexes.put(i, list);
+                        }
+                    } else if (line.charAt(j) == 'R') {
+                        if (vertexes.containsKey(i + j + 1))
+                            vertexes.get(i + j + 1).add(i);
+                        else {
+                            List<Integer> list = new ArrayList<>();
+                            list.add(i);
+                            vertexes.put(i + j + 1, list);
+                        }
+                    }
                 }
-                vertexes.put(i, roads);
             }
         }
-    }
-
-    private static void output(boolean B, boolean R) {
-        if ((B && R) || (!B && !R))
-            System.out.print("NO");
-        else
-            System.out.print("YES");
-
-//        for (Map.Entry entry : vertexes.entrySet()) {
-//            System.out.println(entry.getKey() + " " + entry.getValue());
-//        }
     }
 }
