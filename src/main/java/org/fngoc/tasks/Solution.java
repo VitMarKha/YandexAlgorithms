5a5205ee4dccca45ebf5f6982687562c3f8c89dd
VitMarKha <vitmarkha@icloud.com>
2023-02-09 19:31:23 +0300

    add tasks, in seven sprint

--- src/main/java/org/fngoc/tasks/Solution.java
+++ src/main/java/org/fngoc/tasks/Solution.java
@@ -8,6 +8,69 @@ import java.util.stream.IntStream;
 
 public class Solution {
 
+    ///////////////////Золотая лихорадка///////////////////
+    private static class Heap implements Comparable<Heap>{
+        private final long price;
+        private final long count;
+
+        public Heap(long price, long count) {
+            this.price = price;
+            this.count = count;
+        }
+
+        @Override
+        public int compareTo(Heap o) {
+            return (int) (o.price - this.price);
+        }
+    }
+
+    private static long M;
+    private static Heap[] heaps;
+    private static long resultCountPrice;
+
+    private static void counting() throws IOException {
+        inputCounting();
+        Arrays.sort(heaps);
+
+        for (Heap heap : heaps) {
+            if (M > 0) {
+                for (int i = 0; i < heap.count; i++) {
+                    if (M > 0) {
+                        resultCountPrice += heap.price;
+                        M -= 1;
+                    } else
+                        break;
+                }
+            } else
+                break;
+        }
+        outputCounting();
+    }
+
+    private static void inputCounting() throws IOException {
+        final long N;
+
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            M = Long.parseLong(reader.readLine());
+            N = Long.parseLong(reader.readLine());
+            StringTokenizer tokenizer;
+
+            heaps = new Heap[(int)N];
+
+            for (int i = 0; i < N; i++) {
+                tokenizer = new StringTokenizer(reader.readLine());
+                long price = Long.parseLong(tokenizer.nextToken());
+                long count = Long.parseLong(tokenizer.nextToken());
+
+                heaps[i] = new Heap(price, count);
+            }
+        }
+    }
+
+    private static void outputCounting() {
+        System.out.println(resultCountPrice);
+    }
+
     ///////////////////Расписание///////////////////
     private static class Gap implements Comparable<Gap>{
         private Double start;
