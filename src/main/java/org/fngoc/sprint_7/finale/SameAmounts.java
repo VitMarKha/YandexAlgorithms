2f5ae0a86a125fc9997d0eca86bc774c2cb8b1bd
VitMarKha <vitmarkha@icloud.com>
2023-02-21 23:03:07 +0300

    add final task in 7 sprint

--- src/main/java/org/fngoc/sprint_7/finale/SameAmounts.java
+++ src/main/java/org/fngoc/sprint_7/finale/SameAmounts.java
@@ -13,61 +13,58 @@ public class SameAmounts {
 
     public static void main(String[] args) throws IOException {
         input();
-
-        int sum = Arrays.stream(points).sum();
-        int[] dp = new int[sum + 1];
-        dp[0] = 1;
-
-        for( int i = 0; i < N; i++ )
-            for( int j = sum - points[i]; j >= 0; j--)
-                if(dp[j] == 1)
-                    dp[j + points[i]] = 1;
-
-        System.out.println(Arrays.toString(dp));
-
-//        for (int i = 0; i < N; i++) {
-//            for (int j = N - points[i]; j >= 0; j--) {
-//                if (j == points[i] || j - points[i])
-//            }
-//        }
-
-//        bool partition( vector< int > C ) {
-//            // compute the total sum
-//            int n = C.size();
-//            int N = 0;
-//            for( int i = 0; i < n; i++ ) N += C[i];
-//            // initialize the table
-//            T[0] = true;
-//            for( int i = 1; i <= N; i++ ) T[i] = false;
-
-//            // process the numbers one by one
-//            for( int i = 0; i < n; i++ )
-//                for( int j = N - C[i]; j >= 0; j--)
-//                    if( T[j] ) T[j + C[i]] = true;
-//
-//            for(int i = N/2;i>=0;i--)
-//                if (T[i])
-//                    return i;
-//            return 0;
-//        }
-
-//        for (int i = 0; i < dp.length; i++) {
-//            for (int j = 0; j < dp[i].length; j++) {
-//                System.out.print(dp[i][j]);
-//            }
-//            System.out.println();
-//        }
-
-//        int sum = Arrays.stream(points).sum();
-//        if (dp[] == 1) {
-//            System.out.println("True");
-//        } else
-//            System.out.println("False");
+        output(isTwoSum());
     }
 
-    private static void output() {
-        System.out.println(N);
-        System.out.println(Arrays.toString(points));
+    private static boolean isTwoSum() {
+        int sum = Arrays.stream(points).sum();
+
+        if (sum % 2 != 0)
+            return false;
+
+        int halfSum = sum / 2;
+        boolean[] dp = new boolean[N + 1];
+        dp[0] = true;
+
+        for (int point : points) {
+            if (point == halfSum)
+                return true;
+            if (point > halfSum)
+                return false;
+
+            for (int j = halfSum; j > point - 1; j--) {
+                dp[j] = dp[j - point];
+                if (dp[dp.length - 1])
+                    return true;
+            }
+        }
+        return dp[dp.length - 1];
+
+
+//        int halfSum = sum / 2;
+//        boolean[] dp = new boolean[halfSum + 1];
+//        boolean[] dpPrev = new boolean[halfSum + 1];
+//
+//        for (int i = 1; i < N + 1; i++) {
+//            for (int j = 1; j < halfSum + 1; j++) {
+//                dp[j] = dpPrev[j];
+//
+//                if (j == points[i - 1])
+//                    dp[j] = true;
+//                if (j > points[i - 1] && dpPrev[j - points[i - 1]])
+//                    dp[j] = true;
+//            }
+//            dpPrev = dp;
+//            dp = new boolean[halfSum + 1];
+//        }
+//        return dpPrev[halfSum];
+    }
+
+    private static void output(boolean isTrue) {
+        if (isTrue)
+            System.out.println("True");
+        else
+            System.out.println("False");
     }
 
     private static void input() throws IOException {
@@ -75,10 +72,10 @@ public class SameAmounts {
             StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
             N = Integer.parseInt(tokenizer.nextToken());
 
-            points = new int[N + 1];
+            points = new int[N];
 
             tokenizer = new StringTokenizer(reader.readLine());
-            for (int i = 1; i < N + 1; i++) {
+            for (int i = 0; i < N; i++) {
                 points[i] = Integer.parseInt(tokenizer.nextToken());
             }
         }
